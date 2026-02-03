05888a74f7e26c710755c52678bb22903843eab1
arkadijss <arkadijss91@gmail.com>
2026-02-03 14:27:58 +0100

    Switch to warping in diffusion image coordinates

--- src/warp_and_inpaint.py
+++ src/warp_and_inpaint.py
@@ -17,18 +17,34 @@ def get_depth(depth_dir, frame_id):
     return depth
 
 
-def slice_image_horizontally(image, offset_x, target_w):
-    image_sliced = image[:, offset_x : offset_x + target_w]
-    return image_sliced
+def update_K(cam, orig_shape, tgt_shape):
+    orig_w, orig_h = orig_shape
+    tgt_w, tgt_h = tgt_shape
+    s = tgt_h / orig_h
+    cam.K[0, 0] *= s
+    cam.K[1, 1] *= s
+    new_w = orig_w * s
+    offset_x = (new_w - tgt_w) / 2
+    cam.K[0, 2] = (cam.K[0, 2] * s) - offset_x
+    cam.K[1, 2] *= s
+    return cam, s, offset_x
 
 
-def get_normalized_depth_image(depth_src, max_gen_depth, diffusion_img_shape):
-    depth_clipped = np.clip(depth_src, 0, max_gen_depth)
-    depth_resized = cv2.resize(
-        depth_clipped, diffusion_img_shape, interpolation=cv2.INTER_NEAREST
-    )
-    depth_normalized = depth_resized / max_gen_depth
-    norm_depth_image = (1 - depth_normalized) * 255
+def get_resized_and_cropped_image(
+    image, s, offset_x, tgt_shape, interpolation=cv2.INTER_NEAREST
+):
+    h, w = image.shape[:2]
+    tgt_w, tgt_h = tgt_shape
+    new_w = round(w * s)
+    image_resized = cv2.resize(image, (new_w, tgt_h), interpolation=interpolation)
+    offset_x = round(offset_x)
+    image_cropped = image_resized[:, offset_x : offset_x + tgt_w]
+    return image_cropped
+
+
+def get_normalized_depth_image(depth_image, max_gen_depth):
+    norm_depth_image = depth_image / max_gen_depth
+    norm_depth_image = (1 - norm_depth_image) * 255
     norm_depth_image = norm_depth_image.astype(np.uint8)
     return norm_depth_image
 
@@ -111,13 +127,11 @@ def main():
     variation = "clone"
     cam_id = 0
     frame_ids = [7, 12]
-    depth_thr = 0.2
+    depth_thr = 0.3
     generate_first_frame = True
     max_gen_depth = 80.0
     diffusion_img_shape = (512, 512)  # w, h
     orig_shape = (1242, 375)  # w, h
-    orig_w, orig_h = orig_shape
-    principal_point_offset_x = (orig_w - orig_h) // 2
     opening_kernel_size = 3
     classical_inpaint_kernel_size = 3
     prefill_missing_regions = False
@@ -134,9 +148,8 @@ def main():
     cam = warp_frames_vkitti_2.CameraPerspective(
         vkitti_2_path, location, variation, cam_id
     )
-    # Update principal point in intrinsic matrix for square images
-    cam.K[0, 2] -= principal_point_offset_x
-    print("Principal point offset x:", principal_point_offset_x)
+    # Update intrinsic matrix K for warping in diffusion image coordinates
+    cam, s, offset_x = update_K(cam, orig_shape, diffusion_img_shape)
 
     rel_var_dir = Path(f"Scene{location:02d}") / variation
     rel_frames_dir = rel_var_dir / "frames"
@@ -154,12 +167,17 @@ def main():
         / f"Camera_{cam_id}"
     )
     depth_src = get_depth(depth_dir, src_frame_id)
-    depth_src = slice_image_horizontally(depth_src, principal_point_offset_x, orig_h)
 
     if generate_first_frame:
-        norm_depth_image = get_normalized_depth_image(
-            depth_src, max_gen_depth, diffusion_img_shape
+        depth_clipped = np.clip(depth_src, 0, max_gen_depth)
+        depth_resized = get_resized_and_cropped_image(
+            depth_clipped,
+            s,
+            offset_x,
+            diffusion_img_shape,
+            interpolation=cv2.INTER_NEAREST,
         )
+        norm_depth_image = get_normalized_depth_image(depth_resized, max_gen_depth)
         norm_depth_out_path = output_dir / f"norm_depth_{src_frame_id:05d}.png"
         cv2.imwrite(str(norm_depth_out_path), norm_depth_image)
 
@@ -168,22 +186,16 @@ def main():
         src_frame_gen = generate_sd15.generate_image(
             gen_pipeline, prompt, negative_prompt, depth_image
         )
-        src_frame_out = cv2.cvtColor(np.array(src_frame_gen), cv2.COLOR_RGB2BGR)
-        src_frame = cv2.resize(
-            src_frame_out, (orig_h, orig_h), interpolation=cv2.INTER_LINEAR
-        )
+        src_frame = cv2.cvtColor(np.array(src_frame_gen), cv2.COLOR_RGB2BGR)
     else:
         src_frame_path = rgb_dir / f"rgb_{src_frame_id:05d}.jpg"
         src_frame = cv2.imread(str(src_frame_path))
-        src_frame = slice_image_horizontally(
-            src_frame, principal_point_offset_x, orig_h
-        )
-        src_frame_out = cv2.resize(
-            src_frame, diffusion_img_shape, interpolation=cv2.INTER_LINEAR
+        src_frame = get_resized_and_cropped_image(
+            src_frame, s, offset_x, diffusion_img_shape, interpolation=cv2.INTER_NEAREST
         )
 
     src_frame_out_path = output_dir / f"{src_frame_id:05d}.png"
-    cv2.imwrite(str(src_frame_out_path), src_frame_out)
+    cv2.imwrite(str(src_frame_out_path), src_frame)
 
     # Save output video
     output_video_path = output_dir / f"{experiment_name}.mp4"
@@ -194,14 +206,17 @@ def main():
         fps,
         diffusion_img_shape,
     )
-    video_writer.write(src_frame_out)
+    video_writer.write(src_frame)
 
+    depth_src = get_resized_and_cropped_image(
+        depth_src, s, offset_x, diffusion_img_shape, interpolation=cv2.INTER_NEAREST
+    )
     inpaint_pipeline = generate_sd15.init_inpainting_pipeline()
 
     for tgt_frame_id in frame_ids[1:]:
         depth_tgt = get_depth(depth_dir, tgt_frame_id)
-        depth_tgt = slice_image_horizontally(
-            depth_tgt, principal_point_offset_x, orig_h
+        depth_tgt = get_resized_and_cropped_image(
+            depth_tgt, s, offset_x, diffusion_img_shape, interpolation=cv2.INTER_NEAREST
         )
 
         src_frame_warped, warp_missing_mask = warping_utils.warp_frame(
@@ -259,23 +274,23 @@ def main():
         )
         src_frame_inpainted_diffusion = np.array(src_frame_inpainted_diffusion)
 
-        src_frame_inpainted_diffusion_resized = cv2.resize(
-            src_frame_inpainted_diffusion, (orig_h, orig_h)
-        )
-
         # Prepare depths for interpolation
         interp_ids = np.arange(src_frame_id + 1, tgt_frame_id)
         interp_depths = []
         for frame_id in interp_ids:
             depth_interp = get_depth(depth_dir, frame_id)
-            depth_interp = slice_image_horizontally(
-                depth_interp, principal_point_offset_x, orig_h
+            depth_interp = get_resized_and_cropped_image(
+                depth_interp,
+                s,
+                offset_x,
+                diffusion_img_shape,
+                interpolation=cv2.INTER_NEAREST,
             )
             interp_depths.append(depth_interp)
 
         interpolated_frames = interpolate_frames(
             src_frame,
-            src_frame_inpainted_diffusion_resized,
+            src_frame_inpainted_diffusion,
             src_frame_id,
             tgt_frame_id,
             depth_src,
@@ -328,7 +343,7 @@ def main():
 
         video_writer.write(src_frame_inpainted_diffusion)
 
-        src_frame = src_frame_inpainted_diffusion_resized
+        src_frame = src_frame_inpainted_diffusion
         src_frame_id = tgt_frame_id
         depth_src = depth_tgt
 
